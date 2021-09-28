2ba5aa7be3dde534ab59a70bf91eb620c1371ff1
Jackson Visser <jvisser@pugetsound.edu>
2021-09-28 14:56:50 -0700

    completed initial sorting algorithm. still need to figure out how to properly sort first letter

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 const randWord = dictionary[Math.floor(Math.random() * dictionary.length)];
 alert(`The random word is: ${randWord}`);
+
+function shuffleDurenstenfield(word) {
+  let origWord = populateArray(word);
+
+  let shuffleWord = [];
+  for (let i = origWord.length - 1; i > 0; i--) {
+    let k = getRandIntInc(1, i);
+    let letter = origWord.splice(k, 1);
+    shuffleWord.push(letter);
+  }
+  shuffleWord.push(origWord[0]);
+  return shuffleWord;
+
+  function populateArray(word) {
+    let origWord = [];
+    for (let i = word.length + 1; i >= 0; i--) {
+      origWord.unshift(word.substring(i, i - 1));
+    }
+    origWord.shift();
+    origWord.pop();
+    return origWord;
+  }
+  return populateArray(word);
+}
+
+console.log(
+  `The random word in shuffled array form is ${shuffleDurenstenfield(
+    randWord.toString()
+  )}`
+);
+
+function getRandIntInc(min, max) {
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
